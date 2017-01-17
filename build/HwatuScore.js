86db2b42dc7e464aa9e5705a50812289293bd19c
David Daniel <david.matthew.daniel@gmail.com>
2017-01-16 22:23:25 -0500

    Initial hwatu score commit.

--- build/HwatuScore.js
+++ build/HwatuScore.js
@@ -22,116 +22,131 @@ function l(x) {
   console.log(x);return x;
 }
 
-var Yome = Yome || {};
+var HwatuScore = HwatuScore || {};
 
-Yome.initialState = () => {
+HwatuScore.initialState = () => {
   return { sides: [1, 2, 3, 4, 5, 6, 7, 8].map(() => new Object()) };
 };
 
-Yome.state = Yome.state || Yome.initialState();
-//l(Yome.state)
+HwatuScore.state = HwatuScore.state || HwatuScore.initialState();
+//l(HwatuScore.state)
 
-Yome.sideCount = st => st.sides.length;
-//l(Yome.sideCount(Yome.state))
+HwatuScore.sideCount = st => st.sides.length;
+//l(HwatuScore.sideCount(HwatuScore.state))
 
-Yome.sliceTheta = st => 2 * Math.PI / Yome.sideCount(st);
-//l(Yome.sliceTheta(Yome.state))
+HwatuScore.tally = st => {
+  var numSuit = st.sides.length + 1;
+  var score = 0;
+  if (numSuit == 2) {
+    score = 2;
+  } else if (numSuit == 3) {
+    score = 3;
+  } else if (numSuit == 4) {
+    score = 4;
+  } else if (numSuit == 5) {
+    score = 5;
+  }
+  return score;
+};
+
+HwatuScore.sliceTheta = st => 2 * Math.PI / HwatuScore.sideCount(st);
+//l(HwatuScore.sliceTheta(HwatuScore.state))
 
 
-Yome.rotate = (theta, point) => {
+HwatuScore.rotate = (theta, point) => {
   const sint = Math.sin(theta),
         cost = Math.cos(theta);
   return { x: point.x * cost - point.y * sint,
     y: point.x * sint + point.y * cost };
 };
-//l(Yome.rotate(Math.PI, {x: 0, y: 1}));
+//l(HwatuScore.rotate(Math.PI, {x: 0, y: 1}));
 
-Yome.radialPoint = (radius, theta) => Yome.rotate(theta, { x: 0, y: radius });
-//l(Yome.radialPoint(100, Math.PI));
+HwatuScore.radialPoint = (radius, theta) => HwatuScore.rotate(theta, { x: 0, y: radius });
+//l(HwatuScore.radialPoint(100, Math.PI));
 
-Yome.sidePoints = st => st.sides.map((_, i) => Yome.radialPoint(180, i * Yome.sliceTheta(st)));
-//l(Yome.sidePoints(Yome.initialState()))
+HwatuScore.sidePoints = st => st.sides.map((_, i) => HwatuScore.radialPoint(180, i * HwatuScore.sliceTheta(st)));
+//l(HwatuScore.sidePoints(HwatuScore.initialState()))
 
-Yome.pointsToPointsString = points => points.map(p => p.x + "," + p.y).join(" ");
-//l(Yome.pointsToPointsString(Yome.sidePoints(Yome.initialState())))
+HwatuScore.pointsToPointsString = points => points.map(p => p.x + "," + p.y).join(" ");
+//l(HwatuScore.pointsToPointsString(HwatuScore.sidePoints(HwatuScore.initialState())))
 
-Yome.drawWalls = state => React.createElement("polygon", { points: Yome.pointsToPointsString(Yome.sidePoints(state)) });
+HwatuScore.drawWalls = state => React.createElement("polygon", { points: HwatuScore.pointsToPointsString(HwatuScore.sidePoints(state)) });
 
-Yome.svgWorld = children => React.createElement("svg", { height: "500", width: "500", viewBox: "-250 -250 500 500",
+HwatuScore.svgWorld = children => React.createElement("svg", { height: "500", width: "500", viewBox: "-250 -250 500 500",
   preserveAspectRatio: "xMidYMid meet" }, children);
 
-Yome.playArea = children => ReactDOM.render(Yome.svgWorld(children), document.getElementById("playarea"));
+HwatuScore.playArea = children => ReactDOM.render(HwatuScore.svgWorld(children), document.getElementById("playarea"));
 
-Yome.clearPlayArea = () => ReactDOM.unmountComponentAtNode(document.getElementById("playarea"));
+HwatuScore.clearPlayArea = () => ReactDOM.unmountComponentAtNode(document.getElementById("playarea"));
 
-// Yome.playArea(Yome.drawWalls({sides: [1,2,3,4,5,6,7,8,9,10]}))
-// Yome.playArea(Yome.drawWalls({sides: [1,2,3,4,5,6,7]}))
-// Yome.playArea(Yome.drawWalls({sides: [1,2,3,4,5,6,7,8]}))
+// HwatuScore.playArea(HwatuScore.drawWalls({sides: [1,2,3,4,5,6,7,8,9,10]}))
+// HwatuScore.playArea(HwatuScore.drawWalls({sides: [1,2,3,4,5,6,7]}))
+// HwatuScore.playArea(HwatuScore.drawWalls({sides: [1,2,3,4,5,6,7,8]}))
 
-//Yome.clearPlayArea()
+//HwatuScore.clearPlayArea()
 
-Yome.windowPoints = st => {
-  const theta = Yome.sliceTheta(st),
+HwatuScore.windowPoints = st => {
+  const theta = HwatuScore.sliceTheta(st),
         indent = theta / 6;
-  return [Yome.radialPoint(160, indent), Yome.radialPoint(160, theta - indent), Yome.radialPoint(100, theta / 2)];
+  return [HwatuScore.radialPoint(160, indent), HwatuScore.radialPoint(160, theta - indent), HwatuScore.radialPoint(100, theta / 2)];
 };
-// //l(Yome.windowPoints(Yome.initialState()))
+// //l(HwatuScore.windowPoints(HwatuScore.initialState()))
 //
-Yome.drawWindow = st => React.createElement("polygon", { points: Yome.pointsToPointsString(Yome.windowPoints(st)) });
+HwatuScore.drawWindow = st => React.createElement("polygon", { points: HwatuScore.pointsToPointsString(HwatuScore.windowPoints(st)) });
 //
-// Yome.playArea(<g>{Yome.drawWindow(Yome.initialState())}
-// {Yome.drawWalls(Yome.initialState())}</g>)
+// HwatuScore.playArea(<g>{HwatuScore.drawWindow(HwatuScore.initialState())}
+// {HwatuScore.drawWalls(HwatuScore.initialState())}</g>)
 
-Yome.doorPoints = st => {
-  const indent = Yome.sliceTheta(st) / 8;
-  return [Yome.radialPoint(165, indent), Yome.radialPoint(165, -indent), Yome.radialPoint(90, -indent), Yome.radialPoint(90, indent)];
+HwatuScore.doorPoints = st => {
+  const indent = HwatuScore.sliceTheta(st) / 8;
+  return [HwatuScore.radialPoint(165, indent), HwatuScore.radialPoint(165, -indent), HwatuScore.radialPoint(90, -indent), HwatuScore.radialPoint(90, indent)];
 };
 
-Yome.drawDoor = st => React.createElement("polygon", { points: Yome.pointsToPointsString(Yome.doorPoints(st)) });
+HwatuScore.drawDoor = st => React.createElement("polygon", { points: HwatuScore.pointsToPointsString(HwatuScore.doorPoints(st)) });
 
-//Yome.playArea(<g>{Yome.drawDoor(Yome.state)}
-//                 {Yome.drawWindow(Yome.state)}
-//                 {Yome.drawWalls(Yome.state)}</g>)
+//HwatuScore.playArea(<g>{HwatuScore.drawDoor(HwatuScore.state)}
+//                 {HwatuScore.drawWindow(HwatuScore.state)}
+//                 {HwatuScore.drawWalls(HwatuScore.state)}</g>)
 
-Yome.drawLine = line => React.createElement("line", { x1: line.start.x, y1: line.start.y,
+HwatuScore.drawLine = line => React.createElement("line", { x1: line.start.x, y1: line.start.y,
   x2: line.end.x, y2: line.end.y });
 
-Yome.drawZipDoor = st => {
-  const theta = Yome.sliceTheta(st),
+HwatuScore.drawZipDoor = st => {
+  const theta = HwatuScore.sliceTheta(st),
         indent = 0.15 * (theta / 6),
         lines = [0, 1, 2, 3, 4, 5, 6, 7, 8].map(x => {
     const dist = 170 - 10 * x;
-    return { start: Yome.radialPoint(dist, -indent),
-      end: Yome.radialPoint(dist, indent) };
+    return { start: HwatuScore.radialPoint(dist, -indent),
+      end: HwatuScore.radialPoint(dist, indent) };
   });
-  lines.push({ start: Yome.radialPoint(180, 0),
-    end: Yome.radialPoint(90, 0) });
-  return React.createElement("g", null, lines.map(Yome.drawLine));
+  lines.push({ start: HwatuScore.radialPoint(180, 0),
+    end: HwatuScore.radialPoint(90, 0) });
+  return React.createElement("g", null, lines.map(HwatuScore.drawLine));
 };
 
-//Yome.playArea(<g>{Yome.drawZipDoor(Yome.state)}
-//                 {Yome.drawWalls(Yome.state)}</g>)
+//HwatuScore.playArea(<g>{HwatuScore.drawZipDoor(HwatuScore.state)}
+//                 {HwatuScore.drawWalls(HwatuScore.state)}</g>)
 
-Yome.drawStoveVent = st => {
-  const theta = Yome.sliceTheta(st),
-        point = Yome.radialPoint(155, 0);
+HwatuScore.drawStoveVent = st => {
+  const theta = HwatuScore.sliceTheta(st),
+        point = HwatuScore.radialPoint(155, 0);
   return React.createElement("ellipse", { cx: point.x, cy: point.y, rx: "14", ry: "8",
     key: "stove-vent" });
 };
 
-//Yome.playArea(<g>{Yome.drawStoveVent(Yome.state)}
-//                 {Yome.drawWalls(Yome.state)}</g>)
+//HwatuScore.playArea(<g>{HwatuScore.drawStoveVent(HwatuScore.state)}
+//                 {HwatuScore.drawWalls(HwatuScore.state)}</g>)
 
-Yome.itemRenderDispatch = {
-  "window": Yome.drawWindow,
-  "door-frame": Yome.drawDoor,
-  "zip-door": Yome.drawZipDoor,
-  "stove-vent": Yome.drawStoveVent
+HwatuScore.itemRenderDispatch = {
+  "window": HwatuScore.drawWindow,
+  "door-frame": HwatuScore.drawDoor,
+  "zip-door": HwatuScore.drawZipDoor,
+  "stove-vent": HwatuScore.drawStoveVent
 };
 
-Yome.itemRender = (type, st) => (Yome.itemRenderDispatch[type] || (x => null))(st);
+HwatuScore.itemRender = (type, st) => (HwatuScore.itemRenderDispatch[type] || (x => null))(st);
 
-Yome.exampleData = (state => {
+HwatuScore.exampleData = (state => {
   state.sides[0].face = "window";
   state.sides[0].corner = "zip-door";
   state.sides[3].face = "window";
@@ -139,94 +154,101 @@ Yome.exampleData = (state => {
   state.sides[5].face = "window";
   state.sides[7].corner = "stove-vent";
   return state;
-})(Yome.initialState());
+})(HwatuScore.initialState());
 
-// l(JSON.stringify(Yome.exampleData))
+// l(JSON.stringify(HwatuScore.exampleData))
 
-Yome.sliceDeg = st => 360 / Yome.sideCount(st);
+HwatuScore.sliceDeg = st => 360 / HwatuScore.sideCount(st);
 
-Yome.sideSlice = (st, i) => {
+HwatuScore.sideSlice = (st, i) => {
   const side = st.sides[i];
-  if (side.corner || side.face) return React.createElement("g", { transform: "rotate(" + Yome.sliceDeg(st) * i + ",0,0)" }, Yome.itemRender(side.corner, st), Yome.itemRender(side.face, st));
+  if (side.corner || side.face) return React.createElement("g", { transform: "rotate(" + HwatuScore.sliceDeg(st) * i + ",0,0)" }, HwatuScore.itemRender(side.corner, st), HwatuScore.itemRender(side.face, st));
 };
 
-// Yome.playArea(Yome.sideSlice(Yome.exampleData, 5))
-// Yome.playArea(Yome.sideSlice(Yome.exampleData, 0))
+// HwatuScore.playArea(HwatuScore.sideSlice(HwatuScore.exampleData, 5))
+// HwatuScore.playArea(HwatuScore.sideSlice(HwatuScore.exampleData, 0))
 
-Yome.drawYome = st => React.createElement("g", { transform: "rotate(" + Yome.sliceDeg(st) / 2 + ",0,0)" }, Yome.drawWalls(st), st.sides.map((side, i) => Yome.sideSlice(st, i)));
+HwatuScore.drawHwatuScore = st => React.createElement("g", { transform: "rotate(" + HwatuScore.sliceDeg(st) / 2 + ",0,0)" }, HwatuScore.drawWalls(st), st.sides.map((side, i) => HwatuScore.sideSlice(st, i))
+//st.sideCount
+);
 //
 //
-// Yome.playArea(Yome.drawYome(Yome.exampleData))
-// Yome.clearPlayArea()
+// HwatuScore.playArea(HwatuScore.drawHwatuScore(HwatuScore.exampleData))
+// HwatuScore.clearPlayArea()
 //
 // //side effecting
-Yome.eventHandler = f => e => {
-  e.preventDefault();f(e.target.value);Yome.render();
+HwatuScore.eventHandler = f => e => {
+  e.preventDefault();f(e.target.value).render();
 };
 //
 // //side effecting
-Yome.changeSideCount = new_count => {
+HwatuScore.changeSideCount = new_count => {
   let nArray = Array.apply(null, Array(parseInt(new_count)));
-  Yome.state.sides = nArray.map((_, i) => Yome.state.sides[i] || {});
+  HwatuScore.state.sides = nArray.map((_, i) => HwatuScore.state.sides[i] || {});
 };
-// Yome.changeSideCount(6)
-// Yome.playArea(Yome.drawYome(Yome.state))
-// Yome.changeSideCount(7)
-// Yome.playArea(Yome.drawYome(Yome.exampleData))
-// Yome.changeSideCount(7)
+// HwatuScore.changeSideCount(6)
+// HwatuScore.playArea(HwatuScore.drawHwatuScore(HwatuScore.state))
+// HwatuScore.changeSideCount(7)
+// HwatuScore.playArea(HwatuScore.drawHwatuScore(HwatuScore.exampleData))
+// HwatuScore.changeSideCount(7)
 //
-Yome.sideOptions = () => ["HexaYome", "SeptaYome", "OctaYome"].map((l, v) => React.createElement("option", { value: v + 6 }, l));
+HwatuScore.sideOptions = () => ["1", "2", "3", "4", "5"].map((l, v) => React.createElement("option", { value: v }, l));
 //
-Yome.sideCountInput = st => React.createElement("div", { className: "top-control" }, React.createElement("span", null, " Size of Yome "), React.createElement("select", { onChange: Yome.eventHandler(Yome.changeSideCount),
-  value: Yome.sideCount(st) }, Yome.sideOptions()));
-// ReactDOM.render(Yome.sideCountInput(Yome.state),
+HwatuScore.sideCountInput = st => React.createElement("div", { className: "top-control" }, React.createElement("span", null, " Gwang "), React.createElement("select", { onChange: HwatuScore.eventHandler(HwatuScore.changeSideCount),
+  value: HwatuScore.sideCount(st) }, HwatuScore.sideOptions()));
+// ReactDOM.render(HwatuScore.sideCountInput(HwatuScore.state),
 // document.getElementById("playarea"))
-// Yome.clearPlayArea()
+// HwatuScore.clearPlayArea()
 //
-Yome.worldPosition = point => ({ x: point.x + 250, y: point.y + 250 });
+HwatuScore.worldPosition = point => ({ x: point.x + 250, y: point.y + 250 });
 //
-Yome.addRemoveWindow = i => _ => {
-  const side = Yome.state.sides[i];
+HwatuScore.addRemoveWindow = i => _ => {
+  const side = HwatuScore.state.sides[i];
   side.face = !side.face ? "window" : null;
 };
 
-Yome.windowControl = (st, side, i) => {
-  let theta = Yome.sliceTheta(st) * (i + 1),
-      pos = Yome.worldPosition(Yome.radialPoint(200, theta)),
+HwatuScore.windowControl = (st, side, i) => {
+  let theta = HwatuScore.sliceTheta(st) * (i + 1),
+      pos = HwatuScore.worldPosition(HwatuScore.radialPoint(200, theta)),
       add = !side.face;
   return React.createElement("div", { className: "control-holder", style: { top: pos.y,
       left: pos.x } }, React.createElement("a", { className: "window-control-offset " + (add ? "add" : "remove"),
-    onClick: Yome.eventHandler(Yome.addRemoveWindow(i)),
+    onClick: HwatuScore.eventHandler(HwatuScore.addRemoveWindow(i)),
     href: "#" }, add ? "+ window" : "- window"));
 };
 
-Yome.windowControls = st => st.sides.map((side, i) => Yome.windowControl(st, side, i));
+HwatuScore.windowControls = st => st.sides.map((side, i) => HwatuScore.windowControl(st, side, i));
 //
 // // --- Corner Controls
 //
 // // SIDE EFFECT
-Yome.addRemoveCornerItem = (type, side) => _ => side.corner = side.corner ? null : type;
+HwatuScore.addRemoveCornerItem = (type, side) => _ => side.corner = side.corner ? null : type;
 
-Yome.cornerControlStateClass = (type, corner_type) => !corner_type && "add" || corner_type == type && "remove" || "hidden";
+HwatuScore.cornerControlStateClass = (type, corner_type) => !corner_type && "add" || corner_type == type && "remove" || "hidden";
 
-Yome.cornerControlLink = (type, side) => React.createElement("a", { className: Yome.cornerControlStateClass(type, side.corner),
+HwatuScore.cornerControlLink = (type, side) => React.createElement("a", { className: HwatuScore.cornerControlStateClass(type, side.corner),
   key: type, href: "#",
-  onClick: Yome.eventHandler(Yome.addRemoveCornerItem(type, side)) }, (side.corner ? "- " : "+ ") + type);
+  onClick: HwatuScore.eventHandler(HwatuScore.addRemoveCornerItem(type, side)) }, (side.corner ? "- " : "+ ") + type);
 
-Yome.cornerControlLinks = (side, i) => ["stove-vent", "zip-door", "door-frame"].map(t => Yome.cornerControlLink(t, side));
+HwatuScore.cornerControlLinks = (side, i) => ["stove-vent", "zip-door", "door-frame"].map(t => HwatuScore.cornerControlLink(t, side));
 
-Yome.cornerControl = (st, side, i) => {
-  let theta = Yome.sliceTheta(st) * (i + 0.5),
-      pos = Yome.worldPosition(Yome.radialPoint(221, theta));
-  return React.createElement("div", { className: "control-holder", style: { top: pos.y, left: pos.x } }, React.createElement("div", { className: "corner-control-offset" }, Yome.cornerControlLinks(side, i)));
+HwatuScore.cornerControl = (st, side, i) => {
+  let theta = HwatuScore.sliceTheta(st) * (i + 0.5),
+      pos = HwatuScore.worldPosition(HwatuScore.radialPoint(221, theta));
+  return React.createElement("div", { className: "control-holder", style: { top: pos.y, left: pos.x } }, React.createElement("div", { className: "corner-control-offset" }, HwatuScore.cornerControlLinks(side, i)));
 };
 
-Yome.cornerControls = st => st.sides.map((side, i) => Yome.cornerControl(st, side, i));
+HwatuScore.cornerControls = st => st.sides.map((side, i) => HwatuScore.cornerControl(st, side, i));
 
 // // --- Add new code above this line ---
 //
-Yome.widget = st => React.createElement("div", { className: "yome-widget" }, Yome.sideCountInput(st), React.createElement("div", { className: "yome-widget-body" }, Yome.windowControls(st), Yome.cornerControls(st), Yome.svgWorld(Yome.drawYome(st))));
+HwatuScore.widget = st => React.createElement("div", { className: "HwatuScore-widget" }, HwatuScore.sideCountInput(st), React.createElement("div", { className: "HwatuScore-widget-body" },
+//HwatuScore.windowControls(st),
+//HwatuScore.cornerControls(st),
+//HwatuScore.svgWorld(HwatuScore.drawHwatuScore(st))
+//HwatuScore.sideCount(st)
+HwatuScore.tally(st)));
 //
-Yome.render = () => ReactDOM.render(Yome.widget(Yome.state), document.getElementById('app'));
+HwatuScore.render = () => ReactDOM.render(HwatuScore.widget(HwatuScore.state), document.getElementById('app'));
 //
-Yome.render();
+HwatuScore.render();
